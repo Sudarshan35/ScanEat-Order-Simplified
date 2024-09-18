becd0cd0c44cb9aa60fedd4d9be363c075318b1d
Sudarshan Dhatrak <133034008+Sudarshan35@users.noreply.github.com>
2024-09-19 02:27:00 +0530

    Delete Server directory

--- Server/routes/chefRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/chefRoutes.js
-const express = require('express');
-const chefController = require('../controllers/chefController');
-
-const router = express.Router();
-
-router.put('/chef', chefController.updateOrderStatusToDelivered);
-
-module.exports = router;
